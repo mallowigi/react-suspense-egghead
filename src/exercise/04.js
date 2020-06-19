20b2c924943850a7c29d4917bb134669a01dd80c
Elior <heliosaian@gmail.com>
2020-06-19 22:48:55 +0300

    use within a PokemonCacheProvider

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,6 +2,12 @@ import React from 'react';
 import {fetchPokemon, PokemonDataView, PokemonErrorBoundary, PokemonForm, PokemonInfoFallback} from '../pokemon';
 import {createResource} from '../utils';
 
+/**
+ * Simple component to display a Pokemon
+ * @param pokemonResource The resource promise
+ * @returns {JSX.Element}
+ * @constructor
+ */
 function PokemonInfo({pokemonResource}) {
   const pokemon = pokemonResource.read();
   return (
@@ -14,38 +20,80 @@ function PokemonInfo({pokemonResource}) {
   );
 }
 
+// Wait 4s before showing the fallback, and apply the pending transition for 700ms if the request takes longer than 300ms
 const SUSPENSE_CONFIG = {
   timeoutMs: 4000,
   busyDelayMs: 300,
   busyMinDurationMs: 700,
 };
 
-const pokemonResourceCache = {};
-const PokemonResourceCacheContext = React.createContext(getPokemonResource);
+/**
+ * Create a React Context
+ * @type {React.Context<unknown>}
+ */
+const PokemonResourceCacheContext = React.createContext();
 
-function usePokemonResourceCache() {
-  return React.useContext(PokemonResourceCacheContext);
+/**
+ * Create a Context Provider for the PokemonCache. The provider will provide the getPokemonResource function
+ * @param children
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function PokemonCacheProvider({children}) {
+  const cache = React.useRef({});
+
+  /**
+   * A memoized function to retrieve a pokemon from the Pokemon API and the cache.
+   * @type {function(*): *}
+   */
+  const getPokemonResource = React.useCallback((pokemonName) => {
+    const lowerCaseName = pokemonName.toLowerCase();
+    let resource = cache.current[lowerCaseName];
+    if (!resource) {
+      resource = createPokemonResource(lowerCaseName);
+      cache.current[lowerCaseName] = resource;
+    }
+
+    return resource;
+  }, []);
+
+  return <PokemonResourceCacheContext.Provider value={getPokemonResource}>{children}</PokemonResourceCacheContext.Provider>;
 }
 
+/**
+ * Returns the context value
+ * @returns {unknown}
+ */
+function usePokemonResourceCache() {
+  let context = React.useContext(PokemonResourceCacheContext);
+  if (!context) {
+    throw Error(`usePokemonResourceCache should be used within a PokemonCacheProvider`);
+  }
+  return context;
+}
+
+/**
+ * Create a resource to retrieve a pokemon by name
+ * @param pokemonName
+ * @returns {{read(): (*|undefined)}}
+ */
 function createPokemonResource(pokemonName) {
   return createResource(fetchPokemon(pokemonName));
 }
 
-function getPokemonResource(pokemonName) {
-  const lowerCaseName = pokemonName.toLowerCase();
-  let resource = pokemonResourceCache[lowerCaseName];
-  if (!resource) {
-    resource = createPokemonResource(lowerCaseName);
-    pokemonResourceCache[lowerCaseName] = resource;
-  }
-
-  return resource;
-}
-
+/**
+ * The main element.
+ * It uses Suspense's fallback and transitions to dynamically fetch data.
+ *
+ * The App element should live within a PokemonCacheProvider in order to retrieve the cache.
+ * @returns {JSX.Element}
+ * @constructor
+ */
 function App() {
   const [pokemonName, setPokemonName] = React.useState('');
   const [startTransition, isPending] = React.useTransition(SUSPENSE_CONFIG);
   const [pokemonResource, setPokemonResource] = React.useState(null);
+  // Get the cached getPokemonResource memoized function
   const getPokemonResource = usePokemonResourceCache();
 
   React.useEffect(() => {
@@ -85,4 +133,17 @@ function App() {
   );
 }
 
-export default App;
+/**
+ * The main component wrapped within a PokemonCacheProvider
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function AppWithProvider() {
+  return (
+    <PokemonCacheProvider>
+      <App />
+    </PokemonCacheProvider>
+  );
+}
+
+export default AppWithProvider;
