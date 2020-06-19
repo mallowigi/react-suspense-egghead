b89b6d06ab0c3c76c9820afb6452b3f392bc0e34
Elior <heliosaian@gmail.com>
2020-06-19 16:14:28 +0300

    react suspense start

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,37 +1,14 @@
-// Simple Data-fetching
-// http://localhost:3000/isolated/exercise/01.js
+import React from 'react';
+import {fetchPokemon, PokemonDataView} from '../pokemon';
 
-import React from 'react'
-// 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {PokemonDataView} from '../pokemon'
-
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
-
-// 🐨 create a variable called "pokemon" (using let)
-
-// 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
-}
-
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign the pokemonPromise variable to a call to fetchPokemon('pikachu')
-
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
+let pokemon;
+let promise = fetchPokemon('pikachu').then((pokemonData) => (pokemon = pokemonData));
 
 function PokemonInfo() {
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
+  if (!pokemon) {
+    throw promise;
+  }
 
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
@@ -39,18 +16,19 @@ function PokemonInfo() {
       </div>
       <PokemonDataView pokemon={pokemon} />
     </div>
-  )
+  );
 }
 
 function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <PokemonInfo />
+        <React.Suspense fallback={<div>Loading Pokemon...</div>}>
+          <PokemonInfo />
+        </React.Suspense>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
